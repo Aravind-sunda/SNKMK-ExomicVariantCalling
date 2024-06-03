71b61fef770283750b4a40a5ef7379c7c0bb265c
Aravind Sundar <aravind.sunda@gmail.com>
2024-06-03 18:21:20 -0400

    added a bash script to create the folders in working_directory made changes to snakefile

---
+++ working_directory.bash
@@ -0,0 +1,16 @@
+# 1) Change the following variable to the path of the working directory
+PWD="/restricted/projectnb/cjdgenomics/CJD_GenomicsVariantCalling_20240430"
+
+mkdir -p "$PWD/data"
+mkdir -p "$PWD/reference"
+mkdir -p "$PWD/OUTPUTS-TEMP"
+mkdir -p "$PWD/qsub_logs"
+mkdir -p "$PWD/results"
+mkdir -p "$PWD/analysis"
+# mkdir -p "$PWD/VCF_DB" # Will be created by the command
+
+# 2) Copy the reference genome to the reference directory
+# 3) Copy the WES data to the data directory
+
+# 4) Command to create the samples.txt file. Run this command in the data subdirectory
+ls BOSTON_CJD*_CJD*.aligned.cram | sed 's/\.aligned\.cram//' > samples.txt
