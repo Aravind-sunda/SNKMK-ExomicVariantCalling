eda2a9bbba0dac55758950c41980fe0992e5c1b2
Aravind Sundar <aravind.sunda@gmail.com>
2024-06-04 14:07:50 -0400

    added tempdir to output of markduplicates

--- working_directory.bash
+++ working_directory.bash
@@ -1,12 +1,14 @@
 # 1) Change the following variable to the path of the working directory
 PWD="/restricted/projectnb/cjdgenomics/CJD_GenomicsVariantCalling_20240430"
 
-mkdir -p "$PWD/data"
-mkdir -p "$PWD/reference"
-mkdir -p "$PWD/OUTPUTS-TEMP"
-mkdir -p "$PWD/qsub_logs"
+mkdir -p "$PWD/data" #imp
+mkdir -p "$PWD/reference" #imp
+mkdir -p "$PWD/OUTPUTS-TEMP" #imp
+mkdir -p "$PWD/qsub_logs" #imp
 mkdir -p "$PWD/results"
 mkdir -p "$PWD/analysis"
+
+
 # mkdir -p "$PWD/VCF_DB" # Will be created by the command
 
 # 2) Copy the reference genome to the reference directory
